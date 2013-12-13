f24531abf5c762dda9928b336c17a0ab54b58741
Zane Rockenbaugh <zane@liquid-labs.com>
2013-12-13 20:18:08 +0000

    snapshot help test

--- tests/help.sh
+++ tests/help.sh
@@ -7,10 +7,37 @@
 #* commands managed by git-convey.
 #*
 #* Scenario: Top-level help
+#* Given 'git-convey' is installed
+#* When I type 'git convey help'
+#* Then text is printed to stdout
+#* And the script exits with exit code 0.
 #*
-#* Scenario: Resource help
+#* Scenario: Topics resource help
+#* Given 'git-convey' is installed
+#* When I type 'git convey topics help'
+#* Then text is printed to stdout
+#* And the script exits with exit code 0.
+#*
+#* Scenario: Topics resource help alt
+#* Given 'git-convey' is installed
+#* When I type 'git convey help topics'
+#* Then text is printed to stdout
+#* And the script exits with exit code 0.
+#*
+#* Scenario: Releases resource help
+#* Given 'git-convey' is installed
+#* When I type 'git convey releases help'
+#* Then text is printed to stdout
+#* And the script exits with exit code 0.
+#*
+#* Scenario: Releases resource help alt
+#* Given 'git-convey' is installed
+#* When I type 'git convey help releases'
+#* Then text is printed to stdout
+#* And the script exits with exit code 0.
 #*
 #* Scenario: Task help
+#* Given 'git-convey' is installed
 #* 
 #* </pre>
 #*/

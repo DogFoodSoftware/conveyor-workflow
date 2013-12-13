a482d727c1c2950601dbcf9e5acfca6a4d5c3b8f
Zane Rockenbaugh <zane@liquid-labs.com>
2013-12-13 21:04:15 +0000

    first draft of the help lib

--- tests/help-releases-resource.sh
+++ tests/help-releases-resource.sh
@@ -7,15 +7,20 @@
 #* Scenario: 'git convey releases help'
 #* Given 'git-convey' is installed
 #* When I type 'git convey releases help'
-#* Then text is printed to stdout
+#* Then text is printed to stdout starting with 'usage: git convey release'
+#* And no text is printed to stdeer
 #* And the script exits with exit code 0.
 #*
 #* Scenario: 'git convey help releases'
 #* Given 'git-convey' is installed
 #* When I type 'git convey help releases'
-#* Then text is printed to stdout
+#* Then text is printed to stdout starting with 'usage: git convey release'
+#* And no text is printed to stdeer
 #* And the script exits with exit code 0.
 #* </pre>
 #*/
 
-echo "ERROR: $0 not implemented"
+source `dirname $0`/lib/help-lib.sh
+
+test_help "git convey help releases" "usage: git convey releases"
+test_help "git convey releases help" "usage: git convey releases"

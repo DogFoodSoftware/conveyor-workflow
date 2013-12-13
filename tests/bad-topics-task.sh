dc7ef0dd8dfeceadf7a94f7fd05f8fa5ab69a1ff
Zane Rockenbaugh <zane@liquid-labs.com>
2013-12-13 22:06:33 +0000

    bad topics task test

---
+++ tests/bad-topics-task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#/**
+#* <pre>
+#* Feature: Report bad topics task.
+#*
+#* Scenario: 'git convey topics foo'
+#* Given 'git-convey' is installed
+#* When I type 'git convey topics foo'
+#* Then text is printed to stdout starting with 'usage: git convey topics'
+#* And text is printed to stderr starting with "Unknown task: 'foo'."
+#* And the script exits with exit code 1.
+#* </pre>
+#*/
+
+source `dirname $0`/lib/cli-lib.sh
+
+test_output "git convey topics foo" "usage: git convey topics" "Unknown task: 'foo'." 1

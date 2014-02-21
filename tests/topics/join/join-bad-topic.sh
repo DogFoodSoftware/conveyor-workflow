6814c925ee838c09ae599a749d45e38bddcf4adc
Zane Rockenbaugh <zane@liquid-labs.com>
2014-02-21 20:29:45 +0000

    test for joining bad topic

---
+++ tests/topics/join/join-bad-topic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#/**
+#* <pre>
+#* Feature: Report bad topics branch names on join.
+#*
+#* Scenario: 'git convey topics join bad-topic'
+#* Given 'git-convey' is installed
+#*   And their is no topic 'foo-bar'
+#* When I type 'git convey topics join bad-topic'
+#* Then no text is printed to stdout
+#*   And text is printed to stderr starting with "No such topic 'bad-topic'."
+#*   And the script exits with exit code 1. 
+#* </pre>
+#*/
+
+set -e
+
+source `dirname $0`/../../lib/cli-lib.sh
+setup_path `dirname $0`/../../../runnable
+source `dirname $0`/../../lib/environment-lib.sh
+source `dirname $0`/../../lib/start-lib.sh
+init_test_environment `dirname $0`/../../.. `basename $0`
+cd $WORKING_REPO_PATH
+
+test_output "git convey topics join bad-topic" '' "No such topic 'bad-topic'." 1

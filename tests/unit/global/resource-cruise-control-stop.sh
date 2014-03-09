0b74775b47e704dbdd814ff4be8777c13d42ab5a
Zane Rockenbaugh <zane@liquid-labs.com>
2014-03-09 16:13:56 +0000

    new fully tested

---
+++ tests/unit/global/resource-cruise-control-stop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#/**
+#* <pre>
+#* Feature: Set Resource
+#*
+
+#* Scenario: Implied resource is cleared when non-resource action or another
+#* resource indicated.
+#*
+#* Given 'git-convey' is installed
+#*   And the implied resource has been set to topics
+#*   And I invoke 'con' with a valid resource or global action
+#* When I type 'con checkout foo'
+#* Then I see error "Unknown resource 'checkout'.".
+#* </pre>
+#*/
+TEST_BASE=`dirname $0`/../..
+source $TEST_BASE/lib/cli-lib.sh
+setup_path $TEST_BASE/../runnable
+source $TEST_BASE/lib/environment-lib.sh
+source $TEST_BASE/lib/start-lib.sh
+init_test_environment $TEST_BASE/.. `basename $0`
+cd $WORKING_REPO_PATH
+
+con --setresource topics >/dev/null
+if ! con releases start foo > /dev/null; then
+    echo "ERROR: 'con' execution failed; test inconclusive."
+fi
+test_output 'con checkout task-foo' 'usage: con' "Unknown resource: 'checkout'." 1
+
+con --setresource topics >/dev/null
+if ! con status > /dev/null; then
+    echo "ERROR: 'con' execution failed; test inconclusive."
+fi
+test_output 'con checkout task-foo' 'usage: con' "Unknown resource: 'checkout'." 1
---
+++ tests/unit/global/verify-setresource-message.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#/**
+#* <pre>
+#* Feature: Set Resource
+#*
+#* Scenario: Set resource with long option.
+#* Given 'git-convey' is installed
+#* When I type 'con --setresource topics'
+#* Then text is printed to stdout starting with 'Resource set as 'topics'.'
+#*   And the script exits with value 0.
+#* </pre>
+#*
+#* Repeat for 'releases' and 'issues', and then repeat again with short form.
+#*/
+TEST_BASE=`dirname $0`/../..
+source $TEST_BASE/lib/cli-lib.sh
+setup_path $TEST_BASE/../runnable
+source $TEST_BASE/lib/environment-lib.sh
+source $TEST_BASE/lib/start-lib.sh
+init_test_environment $TEST_BASE/.. `basename $0`
+cd $WORKING_REPO_PATH
+
+test_output 'con --setresource topics' "Resource set as 'topics'." '' 0
+test_output 'con --setresource releases' "Resource set as 'releases'." '' 0
+test_output 'con --setresource issues' "Resource set as 'issues'." '' 0
+test_output 'con -s topics' "Resource set as 'topics'." '' 0
+test_output 'con -s releases' "Resource set as 'releases'." '' 0
+test_output 'con -s issues' "Resource set as 'issues'." '' 0

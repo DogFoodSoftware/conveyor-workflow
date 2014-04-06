aa049673b143629f7c82a7bccd9374a734052ee5
Zane Rockenbaugh <zane@liquid-labs.com>
2014-04-06 03:08:27 +0000

    updated remaining test; possible intermittent race condition with github

--- tests/unit/topics/start/invalid-github-issue.sh
+++ tests/unit/topics/start/invalid-github-issue.sh
@@ -19,15 +19,18 @@
 #*/
 
 TEST_BASE=`dirname $0`/../../..
+TEST_BASE=`realpath $TEST_BASE`
 source $TEST_BASE/lib/cli-lib.sh
 setup_path $TEST_BASE/../runnable
 source $TEST_BASE/lib/environment-lib.sh
 source $TEST_BASE/lib/start-lib.sh
-TEST_REPO='https://github.com/DogFoodSoftware/test-repo.git'
-init_github_test_environment `basename $0` "$TEST_REPO"
+init_github_test_environment `basename $0`
 cd $WORKING_REPO_PATH
 
 ISSUE_DESC=`uuidgen`
 test_output "con topics start 100-$ISSUE_DESC" "" 'GitHub reports invalid issue number at' 1
 # Cleanup branch; actually not necessary if things function right, but just in case.
 git push -q origin :topics-1-$ISSUE_DESC 2>/dev/null
+
+source $TEST_BASE/../runnable/lib/github-hooks.sh
+delete_repo 'DogFoodSoftware/test-repo'

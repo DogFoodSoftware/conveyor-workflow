67ad3c8481522620c33817a540bff943b9b3f481
Zane Rockenbaugh <zane@liquid-labs.com>
2014-04-13 21:54:49 +0000

    got global action test working

---
+++ tests/scenario/globals-do-not-reset-implicit-resource.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#/**
+#* <pre>
+#* Feature: Scenario
+#*
+#* Scenario: User invokes global action without resetting implicit repo
+#*
+#* Given 'conveyor-workflow' is installed
+#*   And a there is a local origin repo
+#* When user executes 'con -s topics; con status; con start --checkout 1-test-topic'
+#* Then topic branch 'topics-1-test-topic' is created
+#*   And the script exits with status 0
+#* </pre>
+#*/
+
+TEST_BASE=`dirname $0`/..
+source $TEST_BASE/lib/cli-lib.sh
+setup_path $TEST_BASE/../runnable
+source $TEST_BASE/lib/environment-lib.sh
+init_test_environment `basename $0`
+cd "$WORKING_REPO_PATH"
+
+con -s topics >/dev/null
+con status > /dev/null
+test_output 'con start --checkout 1-test-topic' "Created topic '1-test-topic'" '' 0

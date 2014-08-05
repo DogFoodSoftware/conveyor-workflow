4af1cfc3c357f976cda75fc59a79d53e9c5cf00d
Zane Rockenbaugh <zane@liquid-labs.com>
2014-08-05 23:51:16 +0000

    went another way with handler paradigm; should simplify; playing with api doc

--- src/rest/requests/patch-item.php
+++ src/rest/requests/patch-item.php
@@ -8,18 +8,13 @@ extract_request_parameters();
 
 # Does the request add a branch?
 if (isset($parameters('branch-name'))) {
-    # The branch name must conform to the work branch spec so that it
-    # can be tied to the request.
-    foo();
 
     # Must define the involved repos.
     if (!isset($parameters['repos'])) {
         final_result_bad_request("Must specify involved repositories with one or more 'repo[]' parameters.")
     }
 
-    foreach ($parameters['repos'] as $repo) {
-        branch_create($repo, $branch_name);
-    }
+
 } # if (isset($parameters['branch-name']))
 ?>
 <?php /**
--- src/rest/requests/start-item.php
+++ src/rest/requests/start-item.php
@@ -1,55 +1,64 @@
 <?php
-
-Rewrite to just do it; we will redirect here from the POST handler, not the other way around. This allows us to do multi-step activities with one call rather than reduce everything to a series of transitions, which is too limiting.
-
 /**
+ * <div id="Request-Spec" class="blurbSummary">
+ * <div class="
+ * </div>
+ * <div id="Implementation" class="blurbSummary">
  * <div class="blurbTitle">Implementation</div>
  */
 require_once('/home/user/playground/dogfoodsoftware.com/conveyor/core/runnable/lib/conveyor-request-lib.php');
 
 extract_request_parameters();
 
+# Verify issue exists.
+bar();
+
 $branch_name = 'requests-'.$item_id;
 if (isset($parameters['branch-label'])) {
     $branch_name .= '-'.$parameters['branch-label'];
     unset($parameters['branch-label']);
 }
-$parameters['branch-name'] = $branch_name;
 
 if (!isset($parameters['assignee'])) {
     if (PHP_SAPI == "cli") {
         $parameters['assignee'] = 'zanerock';
     }
     else {
-        invalid_request();
+        final_result_bad_request("Must specify assignee.");
     }
 }
-if (!isset($parameters['repo'])) {
-    if (PHP_SAPI == "cli") {
-        # Then we check that the current working directory is a within a
-        # local git repo and add that and the origin repo to the
-        # request.
 
-        # At time of writing, the '-q' only works with '--verify'
-        # which requires one parameter so is incompatible with our
-        # command here. Thus, we have to redirect expected error (if we
-        # are not in a git repo) to /dev/null.
-        $path = system('git rev-parse --show-toplevel 2>/dev/null', $retval);
-        if ($retval != 0 || $path == null || trim($path) == "") {
-            final_result_bad_request("Must either explicitly specify repos with one or more 'repo[]' parameters or execute command from a git repository.");
+if (!isset($parameters['primary-repo'])) {
+    if (PHP_SAPI == "cli") {
+        $repo_url = system('git config --get remote.origin.url', $retval);
+        if ($retval == 0 && $repo_url != null && trim($repo_url) != "") {
+            $parameters['primary-repo'] = $repo_url;
         }
-        # else '$path' retrieved.
-        $origin_url = system('git config --get remote.origin.url', $retval);
-        if ($retval != 0 || $origin_url == null || trim($origin_url) == "") {
-            final_result_bad_request("Local git repo '$path' has no 'origin URL' configured; cannot proceed with request.");
-        }
-        # else, good to go
-        $parameters['repo'] = array($path, $origin_url);
     }
-    # else if not operating in a CLI context, then we can leave it to
-    # the PATCH to handle problems.
+    else {
+        final_result_bad_request("Must specify 'primary-repo'.");
+    }
 }
-require("patch-item.php");
+
+if (!isset($parameters['additional-repos']) && PHP_SAPI == "cli") {
+    # At time of writing, the '-q' only works with '--verify'
+    # which requires one parameter so is incompatible with our
+    # command here. Thus, we have to redirect expected error (if we
+    # are not in a git repo) to /dev/null.
+    $repo_path = system('git rev-parse --show-toplevel 2>/dev/null', $retval);
+    if ($retval == 0 && $repo_path != null && trim($repo_path) ! "") {
+        $parameters['additional-repos'] = array($repo_path);
+    }
+}
+
+# The branch name must conform to the work branch spec so that it
+# can be tied to the request.
+foo();
+
+branch_create($parameters['primary-repo'], $branch_name);
+foreach ($parameters['additional-repos'] as $repo) {
+    branch_create($repo, $branch_name);
+}
 ?>
 <?php /**
 </div><!-- .blurbSummary#Implementation -->
